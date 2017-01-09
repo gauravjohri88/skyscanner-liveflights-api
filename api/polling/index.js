d21e80a4c4aa2d7d4f7b73dd3f9e1a43bae1a619
James Hopkins <jameshopkins0709@gmail.com>
2017-01-09 21:34:35 +0000

    Add more polling

--- api/polling/index.js
+++ api/polling/index.js
@@ -1,11 +1,30 @@
-import { after } from 'fluture';
-import { compose, map, path } from 'ramda';
+import Future, { after } from 'fluture';
+import { chain, compose, map, partial, path, propEq } from 'ramda';
 import { format as formatUrl, parse as parseUrl } from 'url';
 
 import { authenticateUrl, getJson, makeRequest} from '../utils';
 
 const addUrlAuthentication = compose(formatUrl, authenticateUrl, parseUrl);
 
-const pollEndpoint = f => f.chain(compose(map(path(['Status'])), getJson, makeRequest()));
+const poll = compose(getJson, makeRequest());
 
-export default compose(pollEndpoint, map(addUrlAuthentication));
+export const pollRes = (rej, res, fetchData, url, data, timeout = setTimeout) => {
+  if (propEq('Status', 'UpdatesComplete', data)) {
+    res(data);
+  } else {
+    timeout(() => fetchData(url)(rej, res), 1000);
+  }
+}
+
+export const fetchData = (url, pollEndpoint = poll) =>
+  (rej, res) => {
+    pollEndpoint(url).fork(
+      rej,
+      partial(pollRes, [rej, res, fetchData, url])
+    )
+  };
+
+export default compose(
+  chain(url => Future(fetchData(url))),
+  map(addUrlAuthentication)
+);
---
+++ api/polling/index.spec.js
@@ -0,0 +1,67 @@
+import assert from 'power-assert';
+import { match, spy } from 'sinon';
+
+import { fetchData, pollRes } from './';
+
+describe('API: Polling', () => {
+  describe('pollRes', () => {
+    it('returns data once the data is ready', () => {
+      const rej = spy();
+      const res = spy();
+      const fetchData = spy();
+      const data = {
+        Status: 'UpdatesComplete'
+      };
+
+      const result = pollRes(rej, res, fetchData, 'url', data);
+      assert(res.calledWith(data));
+    });
+
+    describe('Repolling', () => {
+      it('repolls after 1000ms', () => {
+        const rej = () => {};
+        const res = () => {};
+        const fetchData = () => {};
+        const timeout = spy();
+        const data = {
+          Status: 'UpdatesPending'
+        };
+
+        const result = pollRes(
+          rej,
+          res,
+          fetchData,
+          'http:///www.example.com',
+          data,
+          timeout
+        );
+
+        assert(timeout.calledWith(match.func, 1000));
+      });
+
+      it('calls fetchData to begin loop', () => {
+        const rej = spy();
+        const res = spy();
+        const fetchData2 = spy();
+        const fetchData = spy(() => fetchData2);
+        const timeout = spy();
+        const data = {
+          Status: 'UpdatesPending'
+        };
+
+        const result = pollRes(
+          rej,
+          res,
+          fetchData,
+          'url',
+          data,
+          timeout
+        );
+
+        timeout.args[0][0]();
+        assert(fetchData.calledWith('url'));
+        assert(fetchData2.calledWith(rej, res))
+      });
+    });
+  });
+});
